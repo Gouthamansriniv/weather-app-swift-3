8b9bb44956c8f0ccde06cf00f0f27dfe55d8a29d
Sebastián <jhoansebastianlara@gmail.com>
2017-05-08 17:52:56 -0500

    Cocoa project  pods added

---
+++ Weather Today/CurrentWeather.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentWeather.swift
+//  Weather Today
+//
+//  Created by Sebastián  Lara on 5/6/17.
+//  Copyright © 2017 Sebastián  Lara. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let cityName: String
+    let temperature: Double
+    let weatherCondition: String
+    let humidity: Double
+    let precipitationProbability: Double
+    let pressure: Double
+    let windSpeed: Double
+    let windDeg: Double
+    let icon: String
+}
